5c99e2b628557e3e65378096fa5528b6ad489907
fujie <1243596620@qq.com>
2021-09-11 09:25:34 +0800

    init

---
+++ app/src/main/java/com/app/service/Entry.java
@@ -0,0 +1,10 @@
+package com.app.service;
+
+
+import android.util.Log;
+
+public class Entry {
+    public static void onLoad(ClassLoader classLoader) {
+        Log.i("SharkChilli", "onLoad----------: ");
+    }
+}
